aed714963b75391973519f99eb07c0eb2081fae5
Your Name <you@example.com>
2023-10-26 14:23:42 -0700

    thursday

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,14 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
+
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
+
 module.exports = router;
